9eb45c50b3d976c435bda62ad8f742c779dbacff
Harry <lehuu.hoanlong@gmail.com>
2023-07-12 12:27:57 +0700

    FIX: Fix voice button UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        15: "3.75rem",
+      },
+    },
   },
   plugins: [],
 }
