a0e9e06b46694729a6290cd10926ec5fb83c2090
Thanh <nguyenphucthanh@live.com>
2023-10-24 08:27:06 +0700

    update: more type of quotes

--- src/app/utils/ai.ts
+++ src/app/utils/ai.ts
@@ -5,9 +5,20 @@ export const congrats = async (
   playerNames: string[],
   winner: string
 ): Promise<string | null> => {
+  const random = Math.random() * 100;
+  let demand = "";
+  if (random >= 70) {
+    demand =
+      "Hãy viết một câu mang tính khinh thường 3 người chơi thua trong vòng 20 từ.";
+  } else if (random >= 30) {
+    demand =
+      "Hãy viết một câu khiêu khích hết sức có thể 3 người chơi thua trong vòng 20 từ.";
+  } else {
+    demand = "Hãy viết một câu chúc mừng thật kích động trong vòng 20 từ.";
+  }
   const request = `${playerNames.join(
     ", "
-  )} chơi đánh bài. ${winner} vừa chiến thắng những người chơi còn lại. Hãy viết một câu chúc mừng thật kích động trong vòng 20 từ.`;
+  )} chơi đánh bài. ${winner} vừa chiến thắng những người chơi còn lại. ${demand}`;
 
   const response = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
