6d2f6c6b2244431d075aa86e37434d1c1220eb37
Thanh <nguyenphucthanh@live.com>
2024-05-15 09:22:22 +0700

    feat: change input player type

--- src/components/gamble/PlayerNameInput.tsx
+++ src/components/gamble/PlayerNameInput.tsx
@@ -1,20 +1,29 @@
-import React from "react";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "../ui/select";
+"use client";
+import React, { useCallback, useMemo, useState } from "react";
 import useProfiles from "@/hooks/useUserProfiles";
-import { SelectProps } from "@radix-ui/react-select";
+import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@/components/ui/popover";
+import { Check, ChevronsUpDown } from "lucide-react";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import { cn } from "@/lib/utils";
 
-export type PlayerNameInputProps = SelectProps & {
+export type PlayerNameInputProps = {
   value: string;
   onChange: (value: string) => void;
   placeholder: string;
   name?: string;
-};
+} & React.InputHTMLAttributes<HTMLInputElement>;
 
 export default function PlayerNameInput({
   placeholder,
@@ -24,30 +33,99 @@ export default function PlayerNameInput({
   ...props
 }: PlayerNameInputProps) {
   const profiles = useProfiles();
+  const [open, setOpen] = useState(false);
 
-  const players =
-    profiles?.slice().sort((a, b) => {
-      return a?.name?.localeCompare(b?.name ?? "") ?? 0;
-    }) ?? [];
+  const players = useMemo(() => {
+    return (
+      profiles?.slice().sort((a, b) => {
+        return a?.name?.localeCompare(b?.name ?? "") ?? 0;
+      }) ?? []
+    );
+  }, [profiles]);
+
+  const selectedUser = useMemo(
+    () => profiles?.find((profile) => profile.id === value),
+    [profiles, value],
+  );
+
+  const onFilter = useCallback(
+    (id: string, search: string) => {
+      const found = players?.some((player) => {
+        if (player.id !== id) {
+          return false;
+        }
+        const { name, firstName, lastName } = player;
+        if (
+          name?.toLowerCase().includes(search.toLowerCase()) ??
+          firstName?.toLowerCase().includes(search.toLowerCase()) ??
+          lastName?.toLowerCase().includes(search.toLowerCase())
+        ) {
+          return true;
+        }
+      });
+
+      return found ? 1 : 0;
+    },
+    [players],
+  );
 
   return (
-    <Select
-      value={value}
-      defaultValue={value}
-      onValueChange={onChange}
-      name={name}
-      {...props}
-    >
-      <SelectTrigger className={"w-full"}>
-        <SelectValue placeholder={placeholder} />
-      </SelectTrigger>
-      <SelectContent>
-        {players.map((player) => (
-          <SelectItem key={player.id} value={player.id}>
-            {player.name}
-          </SelectItem>
-        ))}
-      </SelectContent>
-    </Select>
+    <>
+      <input
+        type="hidden"
+        name={name}
+        value={value}
+        data-players={JSON.stringify(players)}
+        {...props}
+      />
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            role="combobox"
+            aria-expanded={open}
+            className="w-full justify-between"
+            type="button"
+            disabled={props.disabled}
+          >
+            {value
+              ? selectedUser?.name ?? selectedUser?.firstName ?? "Unknown user"
+              : placeholder}
+            <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-[200px] p-0">
+          <Command filter={onFilter}>
+            <CommandInput placeholder="Search player..." />
+            <CommandEmpty>No player found.</CommandEmpty>
+            <CommandList>
+              <CommandGroup heading="Players">
+                {players?.map((player) => (
+                  <CommandItem
+                    key={player.id}
+                    value={player.id}
+                    onSelect={(currentValue) => {
+                      onChange(currentValue);
+                      setOpen(false);
+                    }}
+                  >
+                    <Check
+                      className={cn(
+                        "mr-2 h-4 w-4",
+                        value === player.id ? "opacity-100" : "opacity-0",
+                      )}
+                    />
+                    {player.name ??
+                      player.firstName ??
+                      player.lastName ??
+                      "Unknown"}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        </PopoverContent>
+      </Popover>
+    </>
   );
 }
