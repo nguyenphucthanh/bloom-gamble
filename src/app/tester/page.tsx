7137e45a8dc37ba612332712d667c74ff89171a6
Nguyen Phuc Thanh (Thanh Nguyen) <nguyenphucthanh@live.com>
2024-07-04 15:55:01 +0700

    Dev (#29)
    * Feat change select player input (#22)
    
    * feat: change input player type
    
    * update
    
    * fix: build (#24)
    
    * fix: router replace in transition (#26)
    
    * feat: adjust layout (#28)

--- src/app/tester/page.tsx
+++ src/app/tester/page.tsx
@@ -1,11 +1,9 @@
-import Header from "@/components/Header";
 import React from "react";
 import TestMessage from "./test-message";
 
 export default function TesterPage() {
   return (
     <main>
-      <Header title="Tester area" />
       <TestMessage />
     </main>
   );
