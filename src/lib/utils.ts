10361e6e50014cbfc943c46fee47c4b6ca1d3b4d
Nguyen Phuc Thanh (Thanh Nguyen) <nguyenphucthanh@live.com>
2024-07-09 17:33:06 +0700

    release/1.1.7 (#49) (#50)
    * Feat change select player input (#22)
    
    * feat: change input player type
    
    * update
    
    * fix: build (#24)
    
    * fix: router replace in transition (#26)
    
    * feat: adjust layout (#28)
    
    * chore: update package json (#31)
    
    * feat: bet (#32)
    
    * fix: failed build (#35)
    
    * fix: login redirect
    
    * style: update form bet sort player by amount
    
    * style: update
    
    * feat: list bets
    
    * release/1.1.5 (#44)
    
    * Feat change select player input (#22)
    
    * feat: change input player type
    
    * update
    
    * fix: build (#24)
    
    * fix: router replace in transition (#26)
    
    * feat: adjust layout (#28)
    
    * chore: update package json (#31)
    
    * feat: bet (#32)
    
    * fix: failed build (#35)
    
    * fix: login redirect
    
    * style: update form bet sort player by amount
    
    * style: update
    
    * feat: list bets
    
    * fix: failed build
    
    * feat: show BetResult
    
    * feat: format currency

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatCurrency(amount: number, locale = "vi-VN") {
+  return new Intl.NumberFormat(locale, {
+    style: "currency",
+    currency: "VND",
+  }).format(amount)
+}
